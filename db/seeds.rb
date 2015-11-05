0ae2f60ddda94efa102432df6240a7717bb1fe90
Ross Soloff <rsoloff@gmail.com>
2015-11-05 15:21:36 -0500

    built database, models and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Game.create([{ name: 'Super Smash Bros Melee'}, { developer: 'HAL Laboratory' }, { genre: 'Fighting game' }, { publisher: 'Nintendo' }, { image_url: 'https://upload.wikimedia.org/wikipedia/en/7/75/Super_Smash_Bros_Melee_box_art.png' }, { audio_url: "http://downloads.khinsider.com/game-soundtracks/album/super-smash-bros.-melee-original-sound-version/54-menu-1.mp3" }])
+
+Game.create([{ name: 'Blazblue: Chrono Phantasma Extend' }, { developer: 'Arc System Works'}, { genre: 'Fighting game' }, { publisher: 'Aksys Games'}, { image_url: 'http://ecx.images-amazon.com/images/I/81QlyPxvDpL._SL1500_.jpg'}, { audio_url: ''}]), 
